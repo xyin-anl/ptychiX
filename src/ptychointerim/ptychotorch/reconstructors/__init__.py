9a33797b63236506399be093b828ab0e6d8f033a
Hanna Ruth <hruth@pigeon.xray.aps.anl.gov>
2024-10-07 16:55:39 -0500

    REFACTOR: Changed epie.py to pie.py

--- src/ptychointerim/ptychotorch/reconstructors/__init__.py
+++ src/ptychointerim/ptychotorch/reconstructors/__init__.py
@@ -1,7 +1,7 @@
 from .ad_general import AutodiffReconstructor
 from .ad_ptychography import AutodiffPtychographyReconstructor
 from .lsqml import LSQMLReconstructor
-from .epie import PIEReconstructor, EPIEReconstructor, RPIEReconstructor
+from .pie import PIEReconstructor, EPIEReconstructor, RPIEReconstructor
 
 __all__ = [
     'AutodiffReconstructor', 
