c44f2361045551431c4c90af948e72249b7fe81e
Hanna Ruth <hruth@pigeon.xray.aps.anl.gov>
2024-11-11 15:55:46 -0600

    REFACTOR: moved ObjectPatchInterpolator to src/ptychi/image_proc

--- src/ptychi/image_proc.py
+++ src/ptychi/image_proc.py
@@ -7,7 +7,7 @@ from torch import Tensor
 import torch.signal
 
 import ptychi.maths as pmath
-from ptychi.ptychopack.support import ObjectPatchInterpolator
+from ptychi.ptychopack.api import ComplexTensor, RealTensor
 
 
 def extract_patches_fourier_shift(
@@ -135,6 +135,53 @@ def place_patches_fourier_shift(
         pad_lengths[0] : image.shape[1] - pad_lengths[1],
     ]
     return image
+
+
+class ObjectPatchInterpolator:
+    def __init__(self, object_: ComplexTensor, position_px: RealTensor, size: torch.Size) -> None:
+        # top left corner of object support
+        xmin = position_px[-1] - size[-1] / 2
+        ymin = position_px[-2] - size[-2] / 2
+
+        # whole components (pixel indexes)
+        xmin_wh = xmin.int()
+        ymin_wh = ymin.int()
+
+        # fractional (subpixel) components
+        xmin_fr = xmin - xmin_wh
+        ymin_fr = ymin - ymin_wh
+
+        # bottom right corner of object patch support
+        xmax_wh = xmin_wh + size[-1] + 1
+        ymax_wh = ymin_wh + size[-2] + 1
+
+        # reused quantities
+        xmin_fr_c = 1.0 - xmin_fr
+        ymin_fr_c = 1.0 - ymin_fr
+
+        # barycentric interpolant weights
+        self._weight00 = ymin_fr_c * xmin_fr_c
+        self._weight01 = ymin_fr_c * xmin_fr
+        self._weight10 = ymin_fr * xmin_fr_c
+        self._weight11 = ymin_fr * xmin_fr
+
+        # extract patch support region from full object
+        self._object_support = object_[ymin_wh:ymax_wh, xmin_wh:xmax_wh]
+
+    def get_patch(self) -> ComplexTensor:
+        """interpolate object support to extract patch"""
+        object_patch = self._weight00 * self._object_support[:-1, :-1]
+        object_patch += self._weight01 * self._object_support[:-1, 1:]
+        object_patch += self._weight10 * self._object_support[1:, :-1]
+        object_patch += self._weight11 * self._object_support[1:, 1:]
+        return object_patch
+
+    def update_patch(self, object_update: ComplexTensor) -> None:
+        """add patch update to object support"""
+        self._object_support[:-1, :-1] += self._weight00 * object_update
+        self._object_support[:-1, 1:] += self._weight01 * object_update
+        self._object_support[1:, :-1] += self._weight10 * object_update
+        self._object_support[1:, 1:] += self._weight11 * object_update
 
 
 def extract_patches_bilinear_shift(
@@ -958,7 +1005,7 @@ def unwrap_phase_2d(
     Unwrap phase of 2D image.
 
     Parameters
-    ----------
+    ---------- 
     img : Tensor
         A complex 2D tensor giving the image.
     fourier_shift_step : float
