FIX/REFACTOR: updated the denominator in the DM object update to enable good reconstructions using fourier interpolation. Changed DM calculation of object denominator to usethe preconditioner. Changed the preconditioner calculation to use any type of interpolator depending on the option.

src/ptychi/ptychotorch/reconstructors/base.py
@@ -323,16 +323,22 @@ class IterativePtychographyReconstructor(IterativeReconstructor, PtychographyRec
         probe_int = self.parameter_group.probe.get_all_mode_intensity(opr_mode=0)[None, :, :]
         # Shape of probe_int:    (n_scan_points, h, w)
         probe_int = probe_int.repeat(len(positions_all), 1, 1)
+
+        patch_placer = maps.get_patch_interp_function_by_enum(
+            self.parameter_group.object.options.patch_interpolation_method, "placer"
+        )
         # Stitch probes of all positions on the object buffer
         # TODO: allow setting chunk size externally
         probe_sq_map = chunked_processing(
-            func=place_patches_fourier_shift,
+            func=patch_placer,
             common_kwargs={"op": "add"},
             chunkable_kwargs={
                 "positions": positions_all + self.parameter_group.object.center_pixel,
                 "patches": probe_int,
             },
-            iterated_kwargs={"image": torch.zeros_like(object_.real).type(torch.get_default_dtype())},
+            iterated_kwargs={
+                "image": torch.zeros_like(object_.real).type(torch.get_default_dtype())
+            },
             chunk_size=64,
         )
         self.parameter_group.object.preconditioner = probe_sq_map
@@ -519,3 +525,30 @@ class AnalyticalIterativePtychographyReconstructor(
             # Apply object L1-norm constraint.
             if object_.l1_norm_constraint_enabled(self.current_epoch):
                 object_.constrain_l1_norm()
+
+    @staticmethod
+    def replace_propagated_exit_wave_magnitude(
+        psi: Tensor, actual_pattern_intensity: Tensor
+    ) -> Tensor:
+        """
+        Replace the propogated exit wave amplitude.
+
+        Parameters
+        ----------
+        psi : Tensor
+            Predicted exit wave propagated to the detector plane.
+        actual_pattern_intensity : Tensor
+            The measured diffraction pattern at the detector.
+
+        Returns
+        -------
+        Tensor
+            Predicted exit wave with the phase from `psi` and magnitude equal to the square root
+            of `actual_pattern_intensity`.
+        """
+
+        return (
+            psi
+            / ((psi.abs() ** 2).sum(1, keepdims=True).sqrt() + 1e-7)
+            * torch.sqrt(actual_pattern_intensity + 1e-7)[:, None]
+        )

src/ptychi/ptychotorch/reconstructors/dm.py
@@ -45,7 +45,9 @@ class DMReconstructor(AnalyticalIterativePtychographyReconstructor):
         if self.parameter_group.object.is_multislice:
             raise NotImplementedError("DMReconstructor only supports 2D objects.")
         if self.parameter_group.probe.has_multiple_opr_modes:
-            raise NotImplementedError("DMReconstructor does not support multiple OPR modes yet.")
+            raise NotImplementedError(
+                "DMReconstructor does not support multiple OPR modes yet."
+            )
         if (
             self.parameter_group.probe_positions.options.correction_options.correction_type
             != enums.PositionCorrectionTypes.GRADIENT
@@ -87,7 +89,9 @@ class DMReconstructor(AnalyticalIterativePtychographyReconstructor):
         return super().build_loss_tracker()
 
     def run_minibatch(self, input_data, y_true, *args, **kwargs):
-        y_pred = self.compute_updates(*input_data, y_true, self.dataset.valid_pixel_mask)
+        y_pred = self.compute_updates(
+            *input_data, y_true, self.dataset.valid_pixel_mask
+        )
         self.loss_tracker.update_batch_loss_with_metric_function(y_pred, y_true)
 
     def compute_updates(
@@ -150,11 +154,15 @@ class DMReconstructor(AnalyticalIterativePtychographyReconstructor):
             2 * new_psi - self.psi
         )
         revised_psi = torch.where(
-            valid_pixel_mask.repeat(revised_psi.shape[0], self.parameter_group.probe.n_modes, 1, 1),
-            fm.replace_propagated_exit_wave_magnitude(revised_psi, y_true),
+            valid_pixel_mask.repeat(
+                revised_psi.shape[0], self.parameter_group.probe.n_modes, 1, 1
+            ),
+            self.replace_propagated_exit_wave_magnitude(revised_psi, y_true),
             revised_psi,
         )
-        revised_psi = self.forward_model.far_field_propagator.propagate_backward(revised_psi)
+        revised_psi = self.forward_model.far_field_propagator.propagate_backward(
+            revised_psi
+        )
         psi_update = (revised_psi - new_psi) * self.options.exit_wave_update_relaxation
         self.psi += psi_update
 
@@ -182,15 +190,15 @@ class DMReconstructor(AnalyticalIterativePtychographyReconstructor):
             "add",
         )
 
-        object_denominator = torch.zeros_like(object_.get_slice(0), dtype=object_.data.real.dtype)
-        object_denominator = self.patch_placer(
-            object_denominator,
-            positions + object_.center_pixel,
-            (p[None].abs() ** 2).real.sum(1)[0],
-            "add",
-        ).real
+        object_denominator = torch.zeros_like(
+            object_.get_slice(0), dtype=object_.data.real.dtype
+        )
+        self.update_preconditioners()
+        object_denominator = self.parameter_group.object.preconditioner
 
-        updated_object = (object_numerator / (object_denominator + 1e-10))[None]
+        updated_object = object_numerator / torch.sqrt(
+            object_denominator**2 + (1e-3 * object_denominator.max()) ** 2
+        )
 
         # Clamp the object amplitude
         idx = updated_object.abs() > object_.options.amplitude_clamp_limit
@@ -203,9 +211,13 @@ class DMReconstructor(AnalyticalIterativePtychographyReconstructor):
     def update_probe(self, obj_patches: Tensor):
         probe_numerator = (obj_patches.conj() * self.psi).sum(0)
         probe_denominator = (obj_patches.abs() ** 2).sum(0)
-        self.parameter_group.probe.set_data(probe_numerator / (probe_denominator + 1e-10))
+        self.parameter_group.probe.set_data(
+            probe_numerator / (probe_denominator + 1e-10)
+        )
 
-    def update_positions(self, indices: Tensor, obj_patches: Tensor, exit_wave_update: Tensor):
+    def update_positions(
+        self, indices: Tensor, obj_patches: Tensor, exit_wave_update: Tensor
+    ):
         probe = self.parameter_group.probe
         probe_positions = self.parameter_group.probe_positions
 

src/ptychi/ptychotorch/reconstructors/pie.py
@@ -91,7 +91,7 @@ class PIEReconstructor(AnalyticalIterativePtychographyReconstructor):
 
         p = probe.get_opr_mode(0)
 
-        psi_prime = fm.replace_propagated_exit_wave_magnitude(psi_far, y_true)
+        psi_prime = self.replace_propagated_exit_wave_magnitude(psi_far, y_true)
         # Do not swap magnitude for bad pixels.
         psi_prime = torch.where(
             valid_pixel_mask.repeat(psi_prime.shape[0], probe.n_modes, 1, 1), psi_prime, psi_far