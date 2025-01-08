b6e5506d7d688a3e5accb9bce5610eec31eb0776
Ming Du <ming.du101@gmail.com>
2025-01-08 13:51:37 -0600

    FEAT: neural net module

---
+++ src/ptychi/reconstructors/nn/components.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class DoubleConv(nn.Module):
+    """Double convolution layer comprised of
+    ```(Conv2d -> BatchNorm -> ReLU) * 2```
+    
+    This implementation is adapted from https://github.com/milesial/Pytorch-UNet.
+    """
+    def __init__(self, in_channels, out_channels, mid_channels=None):
+        super().__init__()
+        if not mid_channels:
+            mid_channels = out_channels
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(mid_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
